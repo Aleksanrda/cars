568c078eb1a44fdf5a7d068f539cdb9d0561289c
Sasha <oleksandra.kryvko@nure.ua>
2020-10-20 18:21:07 +0300

    #1 Add project skeleton

---
+++ Cars/Cars.Core/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars.Core.Entities
+{
+    public class Car : Entity
+    {
+        public string Name { get; set; }
+
+        public double Volume { get; set; }
+
+        public double Consumption { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Price { get; set; }
+    }
+}
---
+++ Cars/Cars.Core/Repositories/ICarRepository.cs
@@ -0,0 +1,23 @@
+﻿using Cars.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Core.Repositories
+{
+    public interface ICarRepository
+    {
+        Task<IEnumerable<Car>> GetCarsAsync();
+
+        Task<Car> GetCarAsync(int carId);
+
+        Task<Car> AddCarAsync(Car car);
+
+        Task UpdateCarAsync(string carId, Car car);
+
+        Task PatchCarAsync(int carId, Car car);
+
+        Task DeleteCarAsync(string carId);
+    }
+}
